d19fe576d5e43b99ca9125bd5c54a0ce324d1a15
Justin Peisker <justinpeisker@gmail.com>
2022-02-17 13:41:17 -0800

    wip

--- auth-router.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const bcrypt = require('bcryptjs');
-const { JWT_SECRET } = require('./secrets');
-const jwt = require('jsonwebtoken');
-
-module.exports = router;
